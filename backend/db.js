8da2698fc0dc6555a02672eb48a0115864eae9ba
Bharath <bharathseenivasan1@gmail.com>
2026-07-07 23:31:05 +0530

    Debug MySQL env

--- backend/db.js
+++ backend/db.js
@@ -2,11 +2,17 @@ const mysql = require("mysql2");
 require("dotenv").config();
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root9790",
-    database: "smart_bharat"
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE
 });
+
+console.log("MYSQLHOST:", process.env.MYSQLHOST);
+console.log("MYSQLPORT:", process.env.MYSQLPORT);
+console.log("MYSQLUSER:", process.env.MYSQLUSER);
+console.log("MYSQLDATABASE:", process.env.MYSQLDATABASE);
 
 db.connect((err) => {
     if (err) {
